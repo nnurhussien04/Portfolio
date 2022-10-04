92cc9469e1e03df88957b44a20193686a30c4d75
nnurhussien <nnurhussien03@gmail.com>
2022-10-04 20:17:26 +0100

    Update

---
+++ Projects/Java/Tutorial/src/Tutorial.java
@@ -0,0 +1,15 @@
+public class Tutorial {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        int number;
+        number = 5;
+        int number2 = 9;
+        byte b = 120;
+        short snumber = 1000;
+        long lnumber = 234L;
+        float fnumber = 94.5f;
+        double dnumber = 3.67;
+
+
+    }
+}
